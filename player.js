29459928ebab952bb8eded84e4d900883a545319
unknown <sja@ZRHN1454.ads.zuehlke.com>
2014-07-17 16:56:18 +0200

    calculate max bet from all players

--- player.js
+++ player.js
@@ -1,13 +1,26 @@
-
 module.exports = {
 
-  VERSION: "Default JavaScript folding player",
+    VERSION: "1.0",
 
-  bet_request: function(game_state) {
-    return 0;
-  },
+    bet_request: function (game_state) {
+        var findHighestBet = function (game_state) {
+            var max = 0;
+            for (var i = 0; i < game_state.players.length; i++) {
+                if (game_state.players[i].status === 'active') {
+                    if (game_state.players[i].bet > max) {
+                        max = game_state.players[i].bet;
+                    }
+                }
+            }
+            return max;
+        }
 
-  showdown: function(game_state) {
+        console.log('max = ' + findHighestBet(game_state));
 
-  }
+        return 0;
+    },
+
+    showdown: function (game_state) {
+
+    }
 };
