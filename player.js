e37decee628bc93b7978405942d6b3d1f4f560d6
unknown <sja@ZRHN1454.ads.zuehlke.com>
2014-07-17 17:09:07 +0200

    in_action verwendet

--- player.js
+++ player.js
@@ -3,15 +3,7 @@ module.exports = {
     VERSION: "1.0",
 
     bet_request: function (game_state) {
-        var getCurrentPlayer = function(game_state) {
-            for (var i = 0; i < game_state.players.length; i++) {
-                if (game_state.players[i].name === 'Grischa') {
-                    return game_state.players[i];
-                }
-            }
-        }
-
-        return game_state.current_buy_in - getCurrentPlayer(game_state).bet;
+        return game_state.current_buy_in - game_state.players[game_state.in_action].bet;
     },
 
     showdown: function (game_state) {
