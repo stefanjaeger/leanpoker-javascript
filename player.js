782e3e0057353eae0ae7eb087c2bf76300eab7bb
unknown <sja@ZRHN1454.ads.zuehlke.com>
2014-07-17 17:29:05 +0200

    ; vergessen

--- player.js
+++ player.js
@@ -3,7 +3,10 @@ module.exports = {
     VERSION: "1.0",
 
     bet_request: function (game_state) {
-        return game_state.current_buy_in - game_state.players[game_state.in_action].bet;
+        var call = function () {
+            return game_state.current_buy_in - game_state.players[game_state.in_action].bet;
+        }
+        return call();
     },
 
     showdown: function (game_state) {
