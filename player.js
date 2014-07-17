2e751a9d511d9c9e235fab7369567c23a702d597
unknown <sja@ZRHN1454.ads.zuehlke.com>
2014-07-17 20:08:29 +0200

    neue Version :-)

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    VERSION: "Calanda",
+    VERSION: "Desperados",
 
     bet_request: function (game_state) {
         var ourBet = game_state.players[game_state.in_action].bet;
